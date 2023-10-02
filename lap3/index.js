967970a0cf43f26fbfba5154ecf45fc7cd1b7ff6
seta-minhnv <nvminh160899@gmail.com>
2023-10-02 23:30:26 +0700

    update lap 3

--- lap3/index.js
+++ lap3/index.js
@@ -9,3 +9,19 @@ console.log('list', list)
   const handler = [upper.toUpperCase(),...other].join('')
   console.log(`${[first,handler].join('')} ${' *'.repeat(index)}`)
 })
+
+
+// change to red
+
+// change color 
+const h2 = document.querySelector('.to-red')
+const changeColor = (e) => {
+  const value = e.target.innerText
+  const color = `#${Math.round(Math.random() * 1000).toString(16)}`
+  h2.style.cursor = 'pointer'
+  h2.style.color = '#fff'
+  h2.style.backgroundColor = color
+  h2.innerHTML = value
+
+}
+h2.addEventListener('click',changeColor)
